7f7c130220d4d64d815477014230a6373fd6a38d
iamkervyntan <kervyntan@gmail.com>
2022-02-28 16:44:11 +0800

    update (dynamic view)

--- django-project-first/my_site/my_app/urls.py
+++ django-project-first/my_site/my_app/urls.py
@@ -3,10 +3,13 @@ from . import views # to import all the methods in views.py
 # use . to access files in the same folder
 # connect to the views.py
 
-urlpatterns = [
-    path('', views.index, name='index') #/my_apps -- > PROJECT urls.py
-]
+#urlpatterns = [
+#    path('index/', views.index, name='index') #/my_apps -- > PROJECT urls.py
+#]
 
 urlpatterns = [
-    path('', views.simple_view, name="simple_view")
+    path('', views.simple_view, name="simple_view"),
+    path('index/', views.index, name="index"), #directory, call to view, name
+    path('peking/', views.peking_duck, name="peking"),
+    path('<topic>/', views.dynamic_view) ## dynamic view
 ]
--- django-project-first/my_site/my_app/views.py
+++ django-project-first/my_site/my_app/views.py
@@ -3,8 +3,19 @@ from django.http.response import HttpResponse
 
 # Create your views here.
 # view 1
+articles = {
+    'sports' : 'Sports Page',
+    'finance' : "Finance Page"
+}
+
 def index(request):
     return HttpResponse("HELLO THIS IS MY VIEW LOR")
 
 def simple_view (request):
     return HttpResponse("This is my simple_view")
+
+def peking_duck (request):
+    return HttpResponse("I want a peking duck please pronto")
+
+def dynamic_view (request, topic): ### dynamic view
+    return HttpResponse(articles[topic])
